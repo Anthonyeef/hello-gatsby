0b9b301f96e372375c4fb22656c291202e973f8f
yifen <anthonyeef@gmail.com>
2020-03-24 23:01:55 +0800

    style

---
+++ src/pages/about-css-module.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import Header from "../components/header"
+import Container from "../components/container"
+
+import styles from "./about-css-module.module.css"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
+export default () => (
+  <Container>
+    <Header headerTitle="About CSS Modules" />
+    <p>CSS Modules are cool</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum sori amet, constrider lo in er"
+    />
+
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith. Lorem ipsum sori amet, constrider lo in er"
+    />
+  </Container>
+)
